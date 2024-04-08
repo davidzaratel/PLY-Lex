6b8c7356850d016d2163d10c74f18b714616778d
davidzaratel <a01329785@itesm.mx>
2024-04-07 23:54:24 -0600

    question 3, testcase 1

--- clite.py
+++ clite.py
@@ -1,15 +1,22 @@
 import ply.lex as lex
 
-tokens = [ 'INT' ]
+tokens = [ 'INT', 
+            'FLOAT'
+         ]
 
 t_ignore  = ' \t'
+
+
+def t_FLOAT(token):
+    r'[0-9]*(\.[0-9]*)'
+    token.value = float(token.value)
+    return token
 
 def t_INT(token):
     r'[0-9]+([_][0-9]+)*'
     cleanString = token.value.replace("_", "")
     token.value = int(cleanString)
     return token
-
 
 def getLexer():
     return lex.lex()
--- clite_lexer_tests.py
+++ clite_lexer_tests.py
@@ -25,5 +25,51 @@ class TestIntegers(unittest.TestCase):
         self.assertNotEqual(token.value, 1934)
         self.assertRaises(lex.LexError, self.lexer.token)
 
+class TestFloats(unittest.TestCase):
+    def setUp(self):
+        self.lexer = clite.getLexer()
+
+    def test_basic_decimal_floats(self):
+        self.lexer.input('0. 72.40 072.30 2.71828 .25')
+        for value in [0.0, 72.4, 72.3, 2.71828, .25]:
+            token = self.lexer.token()
+            self.assertAlmostEqual(token.value, value)
+            self.assertEqual(token.type, 'FLOAT')
+
+    # def test_basic_decimal_floats_sci_notation(self):
+    #     self.lexer.input('1.e+0 6.67428e-11 1E6 .12345E+5')
+    #     for value in [1.0, 6.67428e-11, 1e6, 0.12345E+5]:
+    #         token = self.lexer.token()
+    #         self.assertAlmostEqual(token.value, value)
+    #         self.assertEqual(token.type, 'FLOAT')
+    
+    # def test_decimal_underscored_floats(self):
+    #     self.lexer.input('1_5. 0.15e+0_2')
+    #     for value in [15.0, 15.0]:
+    #         token = self.lexer.token()
+    #         self.assertAlmostEqual(token.value, value)
+    #         self.assertEqual(token.type, 'FLOAT')
+
+    # def test_wrong_floats_1(self):
+    #     self.lexer.input('1_.5')
+    #     self.lexer.token()  # consumes "1"
+    #     self.assertRaises(lex.LexError, self.lexer.token)
+
+    # def test_wrong_floats_2(self):
+    #     self.lexer.input('1._5')
+    #     self.lexer.token()  # consumes "1."
+    #     self.assertRaises(lex.LexError, self.lexer.token)
+
+    # def test_wrong_floats_3(self):
+    #     self.lexer.input('1.5_e1')
+    #     self.lexer.token()  # consumes "1.5"
+    #     self.assertRaises(lex.LexError, self.lexer.token)
+
+    # def test_wrong_floats_4(self):
+    #     self.lexer.input('1.5e_1')
+    #     self.lexer.token()
+    #     self.assertRaises(lex.LexError, self.lexer.token)
+
 if __name__ == '__main__':
-    unittest.main(TestIntegers())
+    # unittest.main(TestIntegers())
+    unittest.main(TestFloats())
